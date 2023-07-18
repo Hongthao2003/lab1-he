c3981e25ee96030c753a7ea8103faddcef357adf
Hongthao2003 <125128510+Hongthao2003@users.noreply.github.com>
2023-07-18 12:59:31 +0700

    Create co_ban_2.cpp

---
+++ co_ban_2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+ 
+int main(){
+	int ns;
+	cout << "Nhap nam sinh cua ban :";
+	cin >> ns;
+	int tuoi = 2023 - ns;
+	cout << " Ban sinh nam " << ns<< " tuoi ban la :"<< tuoi ;
+	return 0;
+}
