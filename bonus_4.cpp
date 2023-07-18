5185e6b7babb34996a71d961572fcef07b5b220f
Hongthao2003 <125128510+Hongthao2003@users.noreply.github.com>
2023-07-18 15:49:31 +0700

    Create bonus_4.cpp

---
+++ bonus_4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath> 
+using namespace std;
+
+int main(){
+int n, a, tong; 
+	cout << "Nhap hai so nguyen duong: ";
+	cin >> n;
+	for(;n!=0;){
+	a = n%10;
+	tong +=a;
+	n/=10;	
+	}
+	cout << " Tong cac chu so cua la "<< tong << endl;
+	return 0;
+	return 0;
+}  
