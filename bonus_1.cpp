6bea66eedf26c1ef9aa7ec4e0fe85292504f26f1
Hongthao2003 <125128510+Hongthao2003@users.noreply.github.com>
2023-07-18 15:39:01 +0700

    Create bonus_1.cpp

---
+++ bonus_1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath> 
+using namespace std;
+
+int main(){
+int hoten,mssv,lop,sdt,gt;
+cout<< "Ho ten :..................\n";
+cout<< "Ma so sinh vien:..........\n"; 
+cout<< "Lop: .....................\n";
+cout<< "So dien thoai:............\n";
+cout<< "Gioi tinh:................\n"; 
+	return 0;
+} 
