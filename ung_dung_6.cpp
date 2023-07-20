efbc0ebb7204a6e5dd87d6f46a391f92651f5a33
Hongthao2003 <125128510+Hongthao2003@users.noreply.github.com>
2023-07-20 22:38:58 +0700

    Create ung_dung_6.cpp

---
+++ ung_dung_6.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath> 
+using namespace std;
+
+int main(){
+	float a;
+	cout <<" Nhap a :"; cin >> a;
+	double CV = (3*a);
+	cout << " Chu vi tam giac can la:"<< CV; 
+	double DT = a*2*sqrt(3/4);
+	cout<<"\n Dien tich tam giac can la:"<<DT; 
+	return 0; 
+}
